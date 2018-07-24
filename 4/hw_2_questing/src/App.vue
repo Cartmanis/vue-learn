2b79666eca9fcc2d6e5f3c28095e7dbeea2d3fc5
Cartmanis <VShmelcer@gmail.com>
2018-07-25 00:34:48 +0700

    hw_2_questing

---
+++ 4/hw_2_questing/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+      <questing
+      :typ = 'info[0].type' :title = 'info[0].title' :answers= 'info[0].answers'
+      >
+      </questing>
+  </div>
+</template>
+
+<script>
+import Questing from './components/Questing.vue'
+
+export default {
+
+  components: {
+    'questing': Questing
+  },
+  data () {
+    return {
+      info:[
+        {
+            typ: 'radio',
+            title: 'Какой тег задаёт ссылку?',
+            answers: [ 'a', 'div', 'span', 'img']
+        },
+        {
+            typ: 'checkbox',
+            title: 'Какие из этих тегов строчные?',
+            answers: ['a', 'div', 'span', 'img']
+        }
+      ]
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 4/hw_2_questing/src/components/Questing.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>{{title}}</h3>
+    <hr>
+    <div class = 'form-group' v-for = '(answer, index) in answers' :key ='index'>
+      <label for = 'answer'>{{answer}}</label>
+      <input type = 'typ' id = 'answer' value = 'typ'>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title : {
+       type: String,
+       required: true
+    },
+    typ: {
+      type: String,
+      required: true
+    },
+    answers: {
+      type: Array,
+      required: true
+    }
+  },
+}
+</script>
+
